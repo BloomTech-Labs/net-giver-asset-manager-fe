a6e493d0bf51ebd1daaef1eb207b8f88b2d41eab
johnschneider1 <johnlawschneider@gmail.com>
2019-10-14 14:46:39 -0500

    added 2 buttons and stack navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import MainTabNavigator from './MainTabNavigator';
+import MainTabNavigator from "./MainTabNavigator";
+import RegisterScreen from "../screens/RegisterText";
+import LoginScreen from "../screens/LoginText";
+
+// export default createAppContainer(
+//   createSwitchNavigator({
+//     // You could add another route here for authentication.
+//     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+//     Main: MainTabNavigator,
+//   })
+// );
 
 export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+  createStackNavigator(
+    {
+      // You could add another route here for authentication.
+      // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+      Main: MainTabNavigator,
+      Register: RegisterScreen,
+      Login: LoginScreen
+    },
+    {
+      initialRouteName: "Main",
+      defaultNavigationOptions: {
+        title: "Net Giver"
+      }
+    }
+  )
 );
+
+// const navigator = createStackNavigator(
+//   {
+//     Register: RegisterScreen,
+//     Login: LoginScreen
+//   },
+//   {
+//     initialRouteName: 'Main',
+//     defaultNavigationOptions: {
+//       title: "Net Giver"
+//     }
+//   }
+// )
+
+// const App = createAppContainer(navigator);
+
+// export default () => {
+//   return (
+//     <App />
+//   )
+// }
