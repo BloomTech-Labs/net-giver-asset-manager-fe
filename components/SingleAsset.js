47eb5d7b937fc2d271695468fbf35ee8aace162f
Vicky <vyue013@gmail.com>
2019-10-25 09:05:31 -0700

    switched image icon to square

--- components/SingleAsset.js
+++ components/SingleAsset.js
@@ -32,8 +32,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "grey",
     width: 100,
-    height: 100,
-    borderRadius: 100/2
+    height: 100
   },
   textWrapper: {
     flex: 3,
