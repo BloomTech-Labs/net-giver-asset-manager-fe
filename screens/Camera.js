9b73f0e484b0b1ec8212458d49f9b7ca6dbb85c4
ebisLab <ebaiden@techie.com>
2019-10-26 15:29:15 -0400

    flip styling

--- screens/Camera.js
+++ screens/Camera.js
@@ -6,15 +6,27 @@ import { Camera } from 'expo-camera';
 import { AuthSession } from 'expo';
 
 export default class CameraExample extends React.Component {
+    // ref = null;
     state = {
         hasCameraPermission: null,
         type: Camera.Constants.Type.back,
+        previewUri: undefined
     };
 
     async componentDidMount() {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
         this.setState({ hasCameraPermission: status === 'granted' });
     }
+    //------
+    snap = async () => {
+        if (this.camera) {
+            let photo = await this.camera.takePictureAsync();
+        }
+    };
+
+
+
+    ///---
 
     render() {
         const { hasCameraPermission } = this.state;
@@ -25,67 +37,81 @@ export default class CameraExample extends React.Component {
         } else {
             return (
                 <View style={{ flex: 1 }}>
-                    <Camera style={{ flex: 1 }} type={this.state.type}>
+                    <Camera style={{ flex: 1 }} type={this.state.type}
+                        // aspect={Camera.constants.Aspect.fill}
+                        ref={ref => {
+                            this.camera = ref;
+                        }}
+                    >
                         <View
                             style={{
                                 flex: 1,
-                                backgroundColor: 'transparent',
                                 flexDirection: 'row',
-                            }}>
-                            <TouchableOpacity
-                                style={{
-                                    flex: 0.1,
-                                    alignSelf: 'flex-end',
-                                    alignItems: 'center',
-                                }}
-                                onPress={() => {
-                                    this.setState({
-                                        type:
-                                            this.state.type === Camera.Constants.Type.back
-                                                ? Camera.Constants.Type.front
-                                                : Camera.Constants.Type.back,
-                                    });
-                                }}>
-                                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                                {/* <View style={{ alignItems: 'center' }}>
-                                    <FontAwesome style={{ color: 'white', alignItems: 'center' }} name="camera-retro" size={40} />
-                                </View> */}
-                            </TouchableOpacity>
-                            {/* <View style={{
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                marginLeft: 100,
-                                paddingHorizontal: 10, marginBottom: 15,
-                                backgroundColor: 'orange'
-                            }}>
-                                <FontAwesome style={{
-                                    color: 'white', alignItems: 'center', flex: 0.1,
-                                    alignItems: 'flex-end',
-                                }} name="camera-retro" size={40} onPress={() => navigation.navigate("AssetsList")} />
-                            </View> */}
-                        </View>
+                            }}></View>
+
                         <SafeAreaView style={{
-                            color: 'white', alignItems: 'center', flex: 0.1,
+                            color: 'white', alignItems: 'center', flex: 0,
                             alignItems: 'center',
                             marginBottom: 95,
-                            borderRadius: 50,
-                            justifyContent: 'space-between',
-                            paddingBottom: 30
-                        }}>
+                            justifyContent: 'center',
+                            paddingBottom: 30,
+                            flexDirection: 'row',
+                            padding: 10
 
+                        }} >
+                            {/* <View style={{ justifyContent: 'flex-start' }}><Text>Touchable stuff</Text></View> */}
+                            <View
+                                style={{ backgroundColor: 'turqoise' }} >
+                                <TouchableOpacity
+                                    style={{
+                                        // flex: 0.1,
+                                        alignSelf: 'flex-start',
+                                        // alignItems: 'center',
+                                        left: -45
+                                    }}
+                                    onPress={() => {
+                                        this.setState({
+                                            type:
+                                                this.state.type === Camera.Constants.Type.back
+                                                    ? Camera.Constants.Type.front
+                                                    : Camera.Constants.Type.back,
+                                        });
+                                    }}>
+                                    <Text style={{ fontSize: 18, padding: 15, color: 'white' }}>
+                                        <FontAwesome style={{
+                                            color: 'white', alignItems: 'center',
+                                        }} name="undo" size={25} />
+                                    </Text>
+
+                                </TouchableOpacity>
+
+                            </View>
                             <Text style={{
-                                backgroundColor: 'magenta',
+                                backgroundColor: '#3366FF',
                                 borderRadius: 50,
-                                padding: 15
-                            }} onPress={() => navigation.navigate("AssetsList")}>
+                                padding: 15,
+                                left: -25
+                            }}
+                                onPress={this.takePicture.bind(this)}
+                            >
                                 <FontAwesome style={{
-                                    color: 'white', alignItems: 'center',
-                                }} name="camera-retro" size={40} onPress={() => navigation.navigate("AssetsList")} />
+                                    color: 'white', alignItems: 'center'
+                                }} name="check" size={40}
+                                />
                             </Text>
                         </SafeAreaView>
                     </Camera>
                 </View>
             );
         }
+
+
+    }
+
+    takePicture() {
+        const options = {}
+        this.camera.capture({ metadata: options })
+            .then((data => { console.log(data) }))
+            .catch((error) => { console.log(error) })
     }
 }
