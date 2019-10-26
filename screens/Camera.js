81efffd32e0a55d04eeae103b0d1822e121e42d0
ebisLab <ebaiden@techie.com>
2019-10-26 06:46:06 -0400

    button styled

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+import { AuthSession } from 'expo';
 
 export default class CameraExample extends React.Component {
     state = {
@@ -45,8 +47,42 @@ export default class CameraExample extends React.Component {
                                     });
                                 }}>
                                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                                {/* <View style={{ alignItems: 'center' }}>
+                                    <FontAwesome style={{ color: 'white', alignItems: 'center' }} name="camera-retro" size={40} />
+                                </View> */}
                             </TouchableOpacity>
+                            {/* <View style={{
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                marginLeft: 100,
+                                paddingHorizontal: 10, marginBottom: 15,
+                                backgroundColor: 'orange'
+                            }}>
+                                <FontAwesome style={{
+                                    color: 'white', alignItems: 'center', flex: 0.1,
+                                    alignItems: 'flex-end',
+                                }} name="camera-retro" size={40} onPress={() => navigation.navigate("AssetsList")} />
+                            </View> */}
                         </View>
+                        <SafeAreaView style={{
+                            color: 'white', alignItems: 'center', flex: 0.1,
+                            alignItems: 'center',
+                            marginBottom: 95,
+                            borderRadius: 50,
+                            justifyContent: 'space-between',
+                            paddingBottom: 30
+                        }}>
+
+                            <Text style={{
+                                backgroundColor: 'magenta',
+                                borderRadius: 50,
+                                padding: 15
+                            }} onPress={() => navigation.navigate("AssetsList")}>
+                                <FontAwesome style={{
+                                    color: 'white', alignItems: 'center',
+                                }} name="camera-retro" size={40} onPress={() => navigation.navigate("AssetsList")} />
+                            </Text>
+                        </SafeAreaView>
                     </Camera>
                 </View>
             );
