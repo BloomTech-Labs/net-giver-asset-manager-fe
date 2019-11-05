9f63b6ce45f40e9dc938e9ea74aed9b92ad6d4cc
ebisLab <ebaiden@techie.com>
2019-11-05 09:34:32 -0500

    splash page loading tracker

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -2,6 +2,8 @@ import { AsyncStorage } from "react-native";
 import createDataContext from "./createDataContext";
 import assetsApi from "../api/assets";
 import { navigate } from "../navigationRef";
+import ExpoMixpanelAnalytics from '@benawad/expo-mixpanel-analytics';
+const analytics = new ExpoMixpanelAnalytics(process.env.MIXPANEL_SECRET_API_KEY); //planning on putting token it in an env file if it passes
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -33,15 +35,16 @@ const signup = dispatch => async ({ email, password }) => {
 
     navigate("Location");
   } catch (err) {
-    console.log("test context", err);
+    console.log("test context", err);
     dispatch({
       type: "add_error",
-      payload: "Something went wrong with sign up!"
+      payload: "Something went wrong with sign up!"
     });
   }
 };
 
 const signin = dispatch => async ({ email, password, id }) => {
+  // console.log("Btn Clicked")
   try {
     const response = await assetsApi.post("/auth/login", {
       email,
@@ -51,10 +54,12 @@ const signin = dispatch => async ({ email, password, id }) => {
     await AsyncStorage.multiSet([["token", response.data.token], ["user_id", JSON.stringify(response.data.user.id)]])
     dispatch({ type: "signin", payload: response.data.token });
     navigate("App");
+    analytics.track("Logged In", { "Status": "Successful" });
+
   } catch (err) {
     dispatch({
       type: "add_error",
-      payload: "Something went wrong with sign in!"
+      payload: "Something went wrong with sign in!"
     });
   }
 };
