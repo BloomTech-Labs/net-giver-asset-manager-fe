918b2894c539ef06362221053eb09d26aef3fa47
ebisLab <ebaiden@techie.com>
2019-11-18 18:37:45 -0500

    signout

--- screens/SignOut.js
+++ screens/SignOut.js
@@ -4,8 +4,16 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  AsyncStorage
+  AsyncStorage,
+  Image,
+  Platform,
+  SafeAreaView
 } from "react-native";
+import { Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+
+import CustomTabBar from "../components/CustomTabBar"
+
 
 const SignOut = ({ navigation }) => {
   const logOut = async () => {
@@ -13,19 +21,56 @@ const SignOut = ({ navigation }) => {
     navigation.navigate("Landing");
   };
 
+  const leftBtnTxt = "Log Out"
   return (
-    <View>
-      <TouchableOpacity style={styles.touchMe} onPress={logOut}>
-        <Text>OMG GET ME OUT OF THIS APP</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <CustomTabBar leftBtn={leftBtnTxt} />
+
+      <View>
+        <Text style={{
+          fontSize: 33,
+          textAlign: "center",
+          marginTop: "20%",
+        }}>See You Later</Text>
+
+        <Text style={{
+          fontSize: 20,
+          color: "#C0C0C0",
+          textAlign: "center"
+        }}>Are you sure you want to log out?</Text>
+        <Spacer />
+        <Image
+          style={[styles.topImg, {
+            alignSelf: 'center',
+            height: "42%",
+            width: "85%",
+            // borderWidth: 1,
+            // borderRadius: 75
+          }]}
+          source={require("../assets/images/logoutpals.jpg")} />
+        <Spacer />
+        <TouchableOpacity style={styles.touchMe} onPress={logOut}>
+
+
+          {/* <View style={{ marginBottom: backgroundColor: 'pink' }}> */}
+          <Button
+            buttonStyle={[{ marginTop: -30 }, styles.btn]}
+            containerStyle={styles.btnContainer}
+            title="Logout"
+            onPress={}
+          />
+          {/* </View> */}
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+
   );
 };
 
 const styles = StyleSheet.create({
   touchMe: {
     alignSelf: "center",
-    marginTop: 225,
+    // marginTop: 225,
     fontSize: 33
   },
   btn: {
@@ -34,7 +79,18 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: "#3366FF",
     paddingBottom: 8
-  }
+  },
+  topImg: {
+    marginTop: "10%",
+    ...Platform.select({
+      ios: {
+        // marginTop: 5
+      },
+      android: {
+        // marginTop: -20
+      }
+    })
+  },
 });
 
 export default SignOut;
