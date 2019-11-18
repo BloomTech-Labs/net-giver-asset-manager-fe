724e85cf29784771d280c0d9a87972f5672efe96
ebisLab <ebaiden@techie.com>
2019-11-18 14:23:59 -0500

    minor test fix

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
 import renderer from 'react-test-renderer';
-import * as SMSLogin from '../screens/SmsLogin';
+import SMSLogin from '../screens/SmsLogin';
 import LocationForm from '../screens/LocationForm'
-jest.mock('../screens/SmsLogin');
+// jest.mock('../screens/SmsLogin');
 
 // it('renders SMS login page', () => {
 //   const snap = renderer.create(<LocationForm />).toJSON();
