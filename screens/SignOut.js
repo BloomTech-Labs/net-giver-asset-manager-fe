78c3dd1908a10a8f6f196be68abc2a1429d00b64
johnschneider1 <johnlawschneider@gmail.com>
2019-11-18 19:20:01 -0600

    fixed button on signout/logout

--- screens/SignOut.js
+++ screens/SignOut.js
@@ -12,8 +12,7 @@ import {
 import { Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
-import CustomTabBar from "../components/CustomTabBar"
-
+import CustomTabBar from "../components/CustomTabBar";
 
 const SignOut = ({ navigation }) => {
   const logOut = async () => {
@@ -21,49 +20,55 @@ const SignOut = ({ navigation }) => {
     navigation.navigate("Landing");
   };
 
-  const leftBtnTxt = "Log Out"
+  const leftBtnTxt = "Log Out";
   return (
     <SafeAreaView>
       <CustomTabBar leftBtn={leftBtnTxt} />
 
       <View>
-        <Text style={{
-          fontSize: 33,
-          textAlign: "center",
-          marginTop: "20%",
-        }}>See You Later</Text>
+        <Text
+          style={{
+            fontSize: 33,
+            textAlign: "center",
+            marginTop: "20%"
+          }}
+        >
+          See You Later
+        </Text>
 
-        <Text style={{
-          fontSize: 20,
-          color: "#C0C0C0",
-          textAlign: "center"
-        }}>Are you sure you want to log out?</Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "#C0C0C0",
+            textAlign: "center"
+          }}
+        >
+          Are you sure you want to log out?
+        </Text>
         <Spacer />
         <Image
-          style={[styles.topImg, {
-            alignSelf: 'center',
-            height: "42%",
-            width: "85%",
-            // borderWidth: 1,
-            // borderRadius: 75
-          }]}
-          source={require("../assets/images/logoutpals.jpg")} />
+          style={[
+            styles.topImg,
+            {
+              alignSelf: "center",
+              height: "42%",
+              width: "85%"
+              // borderWidth: 1,
+              // borderRadius: 75
+            }
+          ]}
+          source={require("../assets/images/logoutpals.jpg")}
+        />
         <Spacer />
-        <TouchableOpacity style={styles.touchMe} onPress={logOut}>
 
-
-          {/* <View style={{ marginBottom: backgroundColor: 'pink' }}> */}
-          <Button
-            buttonStyle={[{ marginTop: -30 }, styles.btn]}
-            containerStyle={styles.btnContainer}
-            title="Logout"
-          // onPress={}
-          />
-          {/* </View> */}
-        </TouchableOpacity>
+        <Button
+          buttonStyle={[{ marginTop: -30, alignSelf: "center" }, styles.btn]}
+          containerStyle={styles.btnContainer}
+          title="Logout"
+          onPress={logOut}
+        />
       </View>
     </SafeAreaView>
-
   );
 };
 
@@ -90,7 +95,7 @@ const styles = StyleSheet.create({
         // marginTop: -20
       }
     })
-  },
+  }
 });
 
 export default SignOut;
